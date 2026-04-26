bc4834ce2170aff67b285517ec25a065cc3e50f4
finpama <phma020@gmail.com>
2026-04-26 19:20:54 -0300

    finalização da movimentação e interação

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,79 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import Xadrez.Tabuleiro;
 // Pacote contendo regras padrão do xadrez, sobre o tabuleiro e suas peças
 
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void limpaTela() {
+        for (int i = 0; i < 50; ++i) System.out.println();
+    }
+
+    static void main(String[] args) {
         Tabuleiro tabuleiro = new Tabuleiro();
-        tabuleiro.renderizarNoTerminal();
+        Scanner scanner = new Scanner(System.in);
+        String resultadoAnterior = "Primeira rodada";
+        boolean isFim = false;
+
+        limpaTela();
+        System.out.println("  --------------------  Xadrez  -------------------\n");
+        System.out.println("Instruções:");
+        System.out.println(" - Para mover uma peça diga a inicial, a posição onde está, '-' e a posição para onde irá. Ex: Cb2-c3 (Cavalo em b2 para c3).");
+        System.out.println(" - Caso exista uma peça do oponente no destino indicado, haverá uma captura.");
+        System.out.println("\nIniciais (em português)");
+        System.out.println("B - Bispo");
+        System.out.println("C - Cavalo");
+        System.out.println("D - Dama");
+        System.out.println("P - Peão");
+        System.out.println("R - Rei");
+        System.out.println("T - Torre");
+
+        System.out.println("\nPressione enter para continuar...");
+        scanner.nextLine();
+
+        while (true) {
+            limpaTela();
+            System.out.println("  --------------------  Xadrez  -------------------\n");
+            System.out.println("  Resultado última rodada: " + resultadoAnterior);
+            System.out.println("  Rodada atual: " + (tabuleiro.getNumRodadas() + 1) + "\n");
+
+            tabuleiro.renderizarNoTerminal();
+
+            if (tabuleiro.isVezBrancas()) {
+                System.out.print("\nVez das brancas: ");
+            } else {
+                System.out.print("\nVez das pretas: ");
+            }
+
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("exit") || resposta.equals("sair")) {
+                break;
+            }
+
+            Pattern formatoNotacao = Pattern.compile("([BCDPRT][a-h][1-8]\\-[a-h][1-8])", Pattern.CASE_INSENSITIVE);
+            Matcher notacao = formatoNotacao.matcher(resposta);
+
+            if (notacao.find()) {
+                resultadoAnterior = tabuleiro.moverPeca(notacao.group());
+
+            } else {
+                resultadoAnterior = "O movimento não foi escrito na notação correta";
+            }
+
+            if (!tabuleiro.getVencedor().isEmpty()) {
+                break;
+            }
+
+        }
+
+        System.out.println("Jogo finalizado...");
+        if (!tabuleiro.getVencedor().isEmpty()) {
+            System.out.printf("As %s venceram!!!!!%n", tabuleiro.getVencedor());
+        }
 
     }
 }
--- src/Xadrez/Tabuleiro.java
+++ src/Xadrez/Tabuleiro.java
@@ -2,8 +2,28 @@ package Xadrez;
 
 import Xadrez.Pecas.*;
 
+import java.util.Locale;
+
 public class Tabuleiro {
     private Peca[][] matrizPosicoes;
+    private boolean isVezBrancas;
+    private int numRodadas;
+    private String vencedor = "";
+
+    public String getVencedor() {
+        return vencedor;
+    }
+
+    public boolean isVezBrancas() {
+        return isVezBrancas;
+    }
+    public int getNumRodadas() {
+        return numRodadas;
+    }
+    public void proxRodada() {
+        numRodadas++;
+        isVezBrancas = numRodadas % 2 == 0;
+    }
 
     public Peca[][] getMatrizPosicoes() {
         return matrizPosicoes;
@@ -13,6 +33,9 @@ public class Tabuleiro {
     }
 
     public Tabuleiro() {
+        isVezBrancas = true;
+        numRodadas = 0;
+
         setMatrizPosicoes();
         setPecasIniciais();
     }
@@ -95,5 +118,92 @@ public class Tabuleiro {
 
     }
 
+    public String letraParaPeca(String letra) {
+        return switch (letra) {
+            case "B" -> "Bispo";
+            case "C" -> "Cavalo";
+            case "D" -> "Dama";
+            case "P" -> "Peão";
+            case "R" -> "Rei";
+            case "T" -> "Torre";
+            default -> "Desconhecido";
+        };
+    }
+
+    public int letraParaIndex(String letra) {
+        return switch (letra) {
+            case "a" -> 0;
+            case "b" -> 1;
+            case "c" -> 2;
+            case "d" -> 3;
+            case "e" -> 4;
+            case "f" -> 5;
+            case "g" -> 6;
+            case "h" -> 7;
+            default -> -1;
+        };
+    }
+
+    public String moverPeca(String notacao) { // Retorna o resultado do movimento
+        String letraPeca = notacao.substring(0, 1).toUpperCase();
+
+        String str_colInicial = notacao.substring(1, 2);
+        String str_colFinal = notacao.substring(4, 5);
+        String str_linInicial = notacao.substring(2, 3);
+        String str_linFinal = notacao.substring(5);
+
+        int colInicial = letraParaIndex(str_colInicial);
+        int linInicial = Math.abs(Integer.parseInt(str_linInicial) - 8); // inverte a direção da linha para ir de 8 a 1
+
+        int colFinal = letraParaIndex(str_colFinal);
+        int linFinal = Math.abs(Integer.parseInt(str_linFinal) - 8); // inverte a direção da linha para ir de 8 a 1
+
+
+        String notacaoCorrigida = letraPeca + str_colInicial + str_linInicial + "-" + str_colFinal + str_linFinal;
+
+        //checar peça
+
+        try {
+            Peca peca = matrizPosicoes[linInicial][colInicial];
+
+            if (!peca.getNome().equals(letraPeca)) {
+                return "(%s) A peça %s não se encontra na posição especificada.".formatted(notacaoCorrigida, letraParaPeca(letraPeca));
+
+            } else if (peca.isPecaBranca() != isVezBrancas()) {
+                return "(%s) Não é possível mover uma peça rival.".formatted(notacaoCorrigida);
+
+            } else {
+                boolean[][] posicoesPossiveis = peca.isMovimentoValido(linInicial, colInicial);
+
+                if (posicoesPossiveis[linFinal][colFinal]) { // se o movimento para essas coordenadas for possível move a peça
+
+                    if (matrizPosicoes[linFinal][colFinal] != null) {
+                        if (matrizPosicoes[linFinal][colFinal].getNome().equals("R")) {
+                            if (isVezBrancas()) {
+                                vencedor = "Brancas";
+                            } else {
+                                vencedor = "Pretas";
+                            }
+                        }
+                    }
+
+                    matrizPosicoes[linFinal][colFinal] = peca;
+                    matrizPosicoes[linInicial][colInicial] = null;
+
+                    proxRodada();
+                    return "(%s) %s foi movido(a) para %s%s".formatted(notacaoCorrigida, letraParaPeca(peca.getNome()), str_colFinal, str_linFinal);
+
+                } else {
+                    return "(%s) Não é permitido mover a peça %s em %s%s para %s%s".formatted(notacaoCorrigida, letraParaPeca(peca.getNome()), str_colInicial, str_linInicial, str_colFinal, str_linFinal);
+                }
+            }
+
+        } catch (NullPointerException e) {
+            return "(%s) A peça %s não se encontra na posição especificada.".formatted(notacaoCorrigida, letraParaPeca(letraPeca));
+        }
+
+
+
+    }
 
 }
